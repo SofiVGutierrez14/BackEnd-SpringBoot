0331d4c6726bb3c235f653055ef9a41c9b26c232
Sofi V Gutierrez <109046534+SofiVGutierrez14@users.noreply.github.com>
2023-01-16 11:21:36 -0300

    Delete OneDrive directory

--- OneDrive/Escritorio/Sofi/maquetado-sofiaGutierrez/proyectointegrador SofiaGutierrez/Backend/SOFI BACK/Sofi/src/main/java/com/ProyectoSVG/Sofi/Security/Enums/RolNombre.java
+++
@@ -1,7 +0,0 @@
-
-package com.ProyectoSVG.Sofi.Security.Enums;
-
-
-public enum RolNombre {
-    ROLE_ADMIN, ROLE_USER
-}
--- OneDrive/Escritorio/Sofi/maquetado-sofiaGutierrez/proyectointegrador SofiaGutierrez/Backend/SOFI BACK/Sofi/src/main/java/com/ProyectoSVG/Sofi/repository/IEstudioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ProyectoSVG.Sofi.repository;
-import com.ProyectoSVG.Sofi.entity.Estudio;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IEstudioRepository extends JpaRepository <Estudio, Long>{
-    
-}
